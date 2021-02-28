12d1c665a84863a3c9f46794318f9ecc4689002e
joaopedsa <joaopedrosantana1997@hotmail.com.br>
2021-02-27 21:59:20 -0300

    FINISH

--- A2-Socket/client/main.c
+++ A2-Socket/client/main.c
@@ -6,27 +6,45 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void inputOperations(char* operations) {
+    char c;
+    int i = 0;
+
+    printf("Insira os dados para a calculadora:\n");
+    while((c = getchar()) != '\n') {
+        operations[i++] = c;
+    }
+}
+
 int main() {
     int sockfd;
     int len;
     struct sockaddr_in address;
     int result;
-    char ch = 'A';
+    char operations[20];
+
+    if(operations == NULL) {
+        printf("Could not allocate memory!");
+        exit(1);
+    }
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = 9734;
+    address.sin_port = 3456;
     len = sizeof(address);
     result = connect(sockfd, (struct sockaddr *)&address, len);
     if (result == -1) {
         perror("ops: client1");
         exit(1);
     }
-    printf("Connected Client \n");
-    write(sockfd, &ch, 1);
-    read(sockfd, &ch, 1);
-    printf("char from server = %c\n", ch);
+    printf("Connected Client on PORT: %d \n", address.sin_port);
+
+    inputOperations(operations);
+
+    write(sockfd, operations, sizeof(operations));
+    read(sockfd, operations, sizeof(operations));
+    printf("Resultado: %s\n", operations);
     close(sockfd);
     exit(0);
 }
--- A2-Socket/server/main.c
+++ A2-Socket/server/main.c
@@ -6,29 +6,123 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+struct Operation {
+    char operador;
+    char** operandos;
+};
+
+char** getOperandos(char* socketMessage) {
+    char** operandos = NULL;
+    int lenOperando = 0;
+    int numberOperando = 0;
+    operandos = malloc(sizeof(char*));
+    int i = 0;
+    while(socketMessage[i] != '\0') {
+        if(socketMessage[i] != '+' & socketMessage[i] != '-' & socketMessage[i] != '*' & socketMessage[i] != '/') {
+            operandos[numberOperando] = realloc(operandos[numberOperando], sizeof(char));
+            operandos[numberOperando][lenOperando] = socketMessage[i];
+            lenOperando++;
+        } else {
+            operandos = realloc(operandos, sizeof(char*));
+            numberOperando++;
+            lenOperando = 0;
+        }
+        i++;
+    }
+    return operandos;
+}
+
+char getOperador(char* socketMessage) {
+    char operador;
+    int i = 0;
+    while(socketMessage[i] != '\0') {
+        if(socketMessage[i] == '+' | socketMessage[i] == '-' | socketMessage[i] == '*' | socketMessage[i] == '/') {
+            operador = socketMessage[i];
+            break;
+        }
+        ++i;
+    }
+    return operador;
+}
+
+int doOperation(char operation, int number1, int number2) {
+    switch (operation) {
+        case '+':
+            return number1 + number2;
+        case '-':
+            return number1 - number2;
+        case '*':
+            return number1 * number2;
+        case '/':
+            return number1 / number2;
+        default:
+            return 0;
+    }
+}
+
+int calculate(struct Operation operation) {
+    int i = 0;
+    int j = 0;
+    char operador = operation.operador;
+    int number1 = atoi(operation.operandos[0]);
+    int number2 = atoi(operation.operandos[1]);
+    return doOperation(operador, number1, number2);
+}
+
+char* getResult(char* socketMessage) {
+    struct Operation operation;
+    operation.operador = getOperador(socketMessage);
+    operation.operandos = getOperandos(socketMessage);
+
+    int resultInteger = calculate(operation);
+
+    char* result = malloc(20 * sizeof(char));
+
+    sprintf(result, "%d", resultInteger);
+
+    return result;
+}
+
+void clearSpaces(char* socketMessage) {
+    char socketMessageWithoutBlankSpace[250];
+    int i = 0;
+    int j = 0;
+    while(socketMessage[i] != '\0') {
+        if(socketMessage[i] != ' ') {
+            socketMessageWithoutBlankSpace[j] = socketMessage[i];
+            j++;
+        }
+        i++;
+    }
+    socketMessage = socketMessageWithoutBlankSpace;
+}
+
 int main() {
     int server_sockfd, client_sockfd;
     int server_len, client_len;
     struct sockaddr_in server_address;
     struct sockaddr_in client_address;
-    char ch;
+    char socketMessage[250];
+    char* result;
 
     server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = 9734;
+    server_address.sin_port = 3456;
     server_len = sizeof(server_address);
     bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
     listen(server_sockfd, 5);
-    printf("Server Listen\n");
+    printf("Server Listen on PORT: %d\n", server_address.sin_port);
     while(1) {
         client_len = sizeof(client_address);
         client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
-        read(client_sockfd, &ch, 1);
-        printf("Receive Character from client %c\n", ch);
-        ch++;
-        printf("Send Character to client %c\n", ch);
-        write(client_sockfd, &ch, 1);
+        read(client_sockfd, socketMessage, sizeof(socketMessage));
+        printf("Operação Recebida do Cliente: %s\n", socketMessage);
+        result = getResult(socketMessage);
+        printf("Resultado da Operação: %s\n", result);
+        write(client_sockfd, result, sizeof(result));
         close(client_sockfd);
     }
+    close(server_sockfd);
+    exit(0);
 }
